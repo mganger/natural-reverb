3f4b58355507af028334252f41796dac2146e690
Michael Ganger <mganger747@gmail.com>
2020-09-28 17:07:17 -0400

    Updated so that tests are actually run

---
+++ reverb_test.cpp
@@ -0,0 +1,11 @@
+#define BOOST_TEST_MODULE reverb_tests
+#include <boost/test/unit_test.hpp>
+#include <reverb.h>
+
+BOOST_AUTO_TEST_SUITE(reverb_tests)
+
+BOOST_AUTO_TEST_CASE(simple) {
+	BOOST_CHECK(1 == 1);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
